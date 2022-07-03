4a4cdd8b777bacf10541b7e60e82c0002259e1fb
Karel Koudelka <k.koudelka01@gmail.com>
2022-07-03 17:04:48 +0200

    feat: coi problems query

--- src/graphql/resolvers/coiProblem.resolver.ts
+++ src/graphql/resolvers/coiProblem.resolver.ts
@@ -1,5 +1,6 @@
 import { Logger } from "@nestjs/common";
-import { Parent, ResolveField, Resolver } from "@nestjs/graphql";
+import { Args, Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import axios from "axios";
 import { ICoiProblem } from "src/types/coi-problem";
 
 @Resolver("CoiProblem")
@@ -11,5 +12,22 @@ export class CoiProblemresolver {
   @ResolveField("field_datum")
   async fieldDatum(@Parent() coiProblem: ICoiProblem) {
     return new Date(coiProblem.field_datum);
+  }
+
+  @Query("coiProblems")
+  async coiProblems(@Args("maxDate") maxDate: Date): Promise<ICoiProblem[]> {
+    try {
+      const res = await axios.get<ICoiProblem[]>(
+        `https://drupal.kdenatankuju.cz/api/cois/`,
+      );
+
+      if (res.status === 200) {
+        return res.data ?? [];
+      }
+    } catch (e) {
+      this.logger.error(e);
+    }
+
+    return [];
   }
 }
